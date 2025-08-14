12063202017bd43cfd1682ec39f42614c3e6e789
EUNHO CHO <enough1754@gmail.com>
2025-08-15 02:01:10 +0900

    일단 오늘은  product 엔티티만 작업 해두었습니다. 이후 DTO 리포지터리 순으로 작업 진행 예정입니다. 월요일 오전 까지 어떻게든 상품검색및 필터 와 장바구니 까지 구현 예정입니다. 백업 + 작업저장 목적으로 올립니다. 다 완성되면 병합 요청드릴게요

---
+++ src/main/java/com/ballon/domain/product/entity/Product.java
@@ -0,0 +1,67 @@
+package com.ballon.domain.product.entity;
+
+import com.ballon.domain.category.entity.Category;
+import com.ballon.domain.partner.entity.Partner;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product",
+        indexes = {
+            @Index(name = "idx_product_category", columnList = "category_id"),
+            @Index(name = "idx_product_price", columnList = "price"),
+            @Index(name = "idx_product_partner", columnList = "partner_id")
+        })
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Product {
+    public enum Status {ACTIVE, INACTIVE, OUT_OF_STOCK}
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer price;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_product_category"))
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "partner_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_product_partner"))
+    private Partner partner;
+
+    //도메인 변경시 사용 (검증포함) (사용은 서비스 트렌젝셔널에서 사용)
+    public void changeCategory(Category category) {
+        if (category == null) throw new IllegalArgumentException("카테고리는 null일 수 없습니다.");
+        this.category = category;
+    }
+    public void changePrice(Integer price) {
+        if (price < 0) throw new IllegalArgumentException("가격은 0 이상이어야 합니다.");
+        this.price = price;
+    }
+    public void changeQuantity(Integer Quantity) {
+        if (quantity < 0) throw new IllegalArgumentException("수량은 0 이상이어야 합니다.");
+        this.quantity = quantity;
+    }
+    public void changeStatus(Status status) {
+        if (status == null) throw new IllegalArgumentException("상태는 null일 수 없습니다.");
+        this.status = status;
+    }
+
+
+
+
+}
