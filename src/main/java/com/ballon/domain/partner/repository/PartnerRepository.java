9bb28989fc15cc218488d29b3434afd2be5c7815
HwangSlater <doorchan23@naver.com>
2025-09-01 14:31:15 +0900

    수정 사항 - update partner

--- src/main/java/com/ballon/domain/partner/repository/PartnerRepository.java
+++ src/main/java/com/ballon/domain/partner/repository/PartnerRepository.java
@@ -10,8 +10,4 @@ import java.util.Optional;
 public interface PartnerRepository extends JpaRepository<Partner, Long>, CustomPartnerRepository {
     @Query("SELECT p.partnerId FROM Partner p WHERE p.user.userId = :userId")
     Optional<Long> findPartnerIdByUserId(@Param("userId") Long userId);
-
-    Boolean existsByEmail(String email);
-
-    Optional<Long> findPartnerIdByUser_UserId(Long userId);
 }
