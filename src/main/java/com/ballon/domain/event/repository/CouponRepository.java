81d7195411def19789eada4c38f990d230c7fbcd
HwangSlater <doorchan23@naver.com>
2025-09-06 15:35:15 +0900

    수정 사항 - couponRepository 두번 등록 삭제

--- src/main/java/com/ballon/domain/event/repository/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ballon.domain.event.repository;
-
-import com.ballon.domain.coupon.entity.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon,Long> {
-}
