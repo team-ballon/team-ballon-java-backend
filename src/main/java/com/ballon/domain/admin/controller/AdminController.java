64b6d5c21c0158b682a0925e224e714cab6661b9
HwangSlater <doorchan23@naver.com>
2025-10-02 16:32:10 +0900

    수정 사항 - update parameter

--- src/main/java/com/ballon/domain/admin/controller/AdminController.java
+++ src/main/java/com/ballon/domain/admin/controller/AdminController.java
@@ -407,7 +407,7 @@ public class AdminController {
             @ApiResponse(responseCode = "403", description = "권한 없음")
     })
     @GetMapping("/ai-report")
-    public AiReportResponse getAiReportResponseByAiReportType(@RequestParam String aiReportType) {
+    public AiReportResponse getAiReportResponseByAiReportType(@RequestParam("ai_report_type") String aiReportType) {
         return aiReportService.getAiReportResponseByAiReportType(AiReportType.fromValue(aiReportType));
     }
 }
